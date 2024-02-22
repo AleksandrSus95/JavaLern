36246b882968c9b7eb2640a120d092c5dd52798d
aleks <aleksandr.suslov95@gmail.com>
2024-02-22 07:35:06 +0300

    added info about postgres

--- DataBase/PostgreSQL/studyPsqlDay2_2.sql
+++ DataBase/PostgreSQL/studyPsqlDay2_2.sql
@@ -82,3 +82,113 @@ SET ends = '2012-05-04 01:00'
 WHERE title = 'House Party';
 --Проверим что сработал триггер в таблицу logs добавилась строка после обновления таблицы events
 SELECT * FROM logs;
+--Триггеры можно настравить чтобы они срабатывали либо до либо после обновления/вставки в таблицу
+--
+--Представления VIEW
+--Представления являются псевдонимами запросов, позволяют использовать результаты сложного запроса
+--как еще одну таблицу.
+--Определение из инета. Представление - объект базы данных, являющийся результатом выполнения запроса к ней,
+--определенного с помощью оператора SELECT.
+--
+--Синтаксис создания представления:
+--CREATE VIEW имя_представления AS запрос_к_БД_типа_SELECT;
+--Пример
+CREATE VIEW holidays AS
+SELECT event_id AS holiday_id, title AS name, starts AS date
+FROM events
+WHERE title LIKE '%Day%' AND venue_id IS NULL;
+--Теперь представление holidays можно опрашивать как таблицу, но под капотом это все равно таблица events.
+--Представления очень удобны, когда нужно простым спобом получить доступ к результатам сложного запроса.
+--Несколько правил о представлениях
+--1. Любой добавляемый в представление столбец должен присутствовать в базовой таблице
+--2. Обновить представление непосредственно не возможно, т.е использовать функцию UPDATE к представлению не получится
+--   т.к это всего лишь представление (псевдоним запроса) по сути не являющийся таблицей как таковой.
+--   (в этом случае не обойтись без правила) - ЭТО ПОКА НЕ ОЧЕНЬ ПОНЯТНО т.к обновление с помощью представления получилось.
+
+--Изменим таблицу events, добавим колонку цветов ассоциирующихся с праздничным днем
+ALTER TABLE events
+ADD colors text ARRAY;
+--Сразу изменим определяющий представление запрос, включим в него массив colors
+CREATE OR REPLACE VIEW holidays AS
+SELECT event_id AS holiday_id, title AS name, starts AS date, colors
+FROM events
+WHERE title LIKE '%Day%' AND venue_id IS NULL;
+--ОБНОВИТЬ таблицу с помощью представления не выйдет (А вот тут странно так как на самом деле вышло,
+--видимо появилась какаято фича)
+UPDATE holidays SET color = '{"red","green"}' WHERE name = 'Christmas Day';
+--Должен был выдать
+--ERROR: cannot update a view
+--HINT: You need an unconditional ON UPDATE DO INSTEAD rule.
+
+--Правила RULE
+--Правила - это описание способа изменения разобранного дерева запроса. каждый раз когда выполняется команда SQL
+--порождается дерево запроса (или его еще называются абстрактным синтаксическим деревом).
+--
+--Чтобы посмотреть план выполнения запроса используют команду EXPLAIN (фильтр - это фраза WHERE, а OUTPUT - это список столбцов).
+--
+--Пример команды EXPLAIN
+EXPLAIN VERBOSE
+SELECT * FROM holidays;
+--После выполнения это команды SQL выдаст план выполнения запроса.
+--По идее чтобы разрешить обновление представления см. выше holidays мы должны объяснить постгрес что делать с
+--командой UPDATE и представлением. Правило будет "запоминать" все обновления holidays и применять их к таблице
+--events, получая значения из псевдоотношений NEW и OLD. Функционально NEW - интерпритируется как отношение, содержащие
+--новые значения, а OLD - как отношение, содержащее значение старое (до обновления).
+--
+--Пример создания правила для представления (VIEW) - holidays.
+CREATE RULE update_holidays AS ON UPDATE TO holidays DO INSTEAD
+UPDATE events
+SET title = NEW.name,
+    starts = NEW.date,
+    colors = NEW.colors
+    WHERE title = OLD.name;
+--Создав такое правило мы можем обновить таблицу events с помощью представления holidays
+UPDATE holidays SET colors = '{"red","green"}' where name = 'Christmas Day';
+
+--Пример не работающией вставки в представление
+INSERT INTO holidays (title, starts, ends) VALUES ('New Year','2012-12-31 00:00','2013-01-31 23:59');
+
+--Обновим представление
+CREATE OR REPLACE VIEW holidays AS
+SELECT event_id AS holiday_id, title AS name, starts AS date, colors, ends AS dateEnd
+FROM events
+WHERE title LIKE '%Day%' AND venue_id IS NULL;
+
+--Создание правила для вставки INSERT
+CREATE RULE insert_holidays AS ON INSERT TO holidays DO INSTEAD
+INSERT INTO events (title, starts, colors, ends)
+VALUES (NEW.name, NEW.date, NEW.colors, NEW.dateEnd);
+
+--Создание сводных таблиц с помщью crosstab()
+--Создадим календарь мероприятий где для каждого месяца в году будет подсчитано количество мероприятий в этом месяце
+--Сводная таблица - сводит сгруппированные данные относительно результатов какой-то другой операции (в этом случае списка месяцев).
+--Строится в постгрес с помощью функции crosstab().
+--
+--Еще одна функция - extract() выделяет компоненты даты или времени.
+
+SELECT extract(year from starts) as year, extract(month from starts) as month, count(*)
+FROM events
+GROUP BY year, month;
+
+--Чтобы воспользоваться функцией crosstab(), запрос должен возвращать 3 столбца
+--1. Идентификатор строки - у нас будет год
+--2. Категория - в качестве категории у нас будет месяц
+--3. Значение - в качестве значения счетчик count()
+--Создадим временную таблицу
+CREATE TEMPORARY TABLE month_count (month INT);
+INSERT INTO month_count VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12);
+
+-- Если он выдаст какието проблемы с функцией crosstab() то создать расширение
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+--Вызовем функцию crosstab()
+SELECT * FROM crosstab(
+'SELECT extract(year from starts) as year,
+        extract(month from starts) as month,
+        count(*)
+ FROM events
+ GROUP BY year, month',
+ 'SELECT * FROM month_count') AS (
+ year INT,
+ jan INT, feb INT, mar INT, apr INT, may INT, jun INT, jul INT, aug INT, sep INT, oct INT, nov INT, dec int)
+ ORDER BY YEAR;
