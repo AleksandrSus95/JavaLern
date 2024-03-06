3233428a85e91785159c64fc6344ee146dd43c0c
aleks <aleksandr.suslov95@gmail.com>
2024-03-06 07:45:19 +0300

    add arrayList notes and example

--- JavaLernBase/src/test/java/collectionJava/LearnCollectionArrayTest.java
+++ JavaLernBase/src/test/java/collectionJava/LearnCollectionArrayTest.java
@@ -3,12 +3,60 @@ package collectionJava;
 import collectionModels.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import static java.time.LocalDate.now;
 import static java.util.UUID.randomUUID;
 
 public class LearnCollectionArrayTest {
+
+    @Test
+    public void collectionArrayList() {
+        /*
+        АррэйЛист или массив переменной длины, используется тогда, когда нужна структура похожая на массив,
+        другими словами АррэйЛист - массив изменяющий свою длину.
+
+        в ArrayList есть понятие capasity см. в исходниках, с помощью него инициализируется изначальный массив объектов
+        */
+        // Способы создания АррэйЛист
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("Aleksandr");
+        list1.add("Ivan");
+        list1.add("Irina");
+        list1.add("Ibragim");
+        System.out.println(list1);
+        // Создания без второго указания типа данных ArrayList<String> list = new ArrayList<>();
+        // Можно указать размер массива АррэйЛиста указав размер в конструкторе: ArrayList<String> list = new ArrayList<>(200);
+        // Можно создать на базе другого АррэйЛиста:  ArrayList<String> list = new ArrayList<>(arrayList);
+        // Если создать список без указания типа в дженериках, то в лист можно будет добавлять любые данные,
+        // т.к АррэйЛист в своей основе содержит тип данных Object
+        list1.add(1, "Nikolay"); // Добавит в позицию под индексом один новое значение, но не затрет предыдущие, а сдвинет их вперед
+        System.out.println(list1); // НО таким способом нельзя добавить в новую позицию т.к если у нас размер равен 3 то 4ой ячейки еще не существует
+        list1.set(1, "New Name"); // set заменит элемент по указанному индексу
+        System.out.println(list1);
+        list1.remove("Ivan"); // Удалит элемент если найдет совпадение по указанному объекту, будет удален первый попавшийся элемент совпадающий с указанным, вернет тип булеан в слуаче успеха труе
+        // Для корректной работы метода выше надо чтобы был перепорпеделен метод equals (базовый equals в своей сути использует == и будет труе если ссылки совпадают, значение при этом проверяться не будет)
+        System.out.println(list1);
+        list1.remove(3); // Удалит элемент по указанному индексу и вернет удаленный элемент )
+        System.out.println(list1);
+
+        ArrayList<String> list2 = new ArrayList<>(){{
+           add("Misha");
+           add("Viktor");
+           add("Marina");
+        }};
+
+        list1.addAll(list2); // Добавляет все элементы в конец списка, так же можно добавить новые элементы с определенного индекса
+        System.out.println(list1);
+        System.out.println(list1.indexOf("Misha")); // Поиск индекса элемента, он так же покажет первый найденный элемент
+        System.out.println(list1.contains("Marina")); // Проверит содержит ли список указанный элемент, будет использовать для сравнения equals
+        list1.containsAll(list2); // Проверит содержит ли список все элементы переданной в метод коллекции
+        list1.lastIndexOf("Object"); // Поиск индекса элемента, найден элемент по последнему совпадению
+        list1.clear(); // Опустошит список
+        list1.isEmpty(); // Покажет пуст ли наш список (фолсе если не пуст, труе если пустой)
+    }
+
 
     @Test
     public void collectionLinkedListTest() {
--- JavaLernBase/src/test/java/exampleUsingJava/ExampleStreamApi.java
+++ JavaLernBase/src/test/java/exampleUsingJava/ExampleStreamApi.java
@@ -193,8 +193,7 @@ public class ExampleStreamApi {
     @Description("Примеры использования методов класса Collectors в StreamAPI")
     public void exampleUsingCollectors(){
         List<String> strings = List.of("as a the d on and".split("\\s+"));
-        // toCollection(Supplier<C> collectionFactory), toList(), toSet() — преобразо-
-        // вание в коллекцию;
+        // toCollection(Supplier<C> collectionFactory), toList(), toSet() — преобразование в коллекцию;
         List<Integer> listLengths = strings.stream()
                 .map(s -> s.length())
                 .collect(Collectors.toList());
@@ -204,8 +203,7 @@ public class ExampleStreamApi {
                 .map(s -> s.charAt(0))
                 .collect(Collectors.toCollection(ArrayList::new));
         System.out.println(listFirstSymbol);
-        // joining(CharSequence delimiter) —обеспечивает конкатенацию строк с за-
-        // данным разделителем;
+        // joining(CharSequence delimiter) —обеспечивает конкатенацию строк с заданным разделителем;
         String results = strings.stream()
                 .map(String::toUpperCase)
                 .collect(Collectors.joining(":"));
@@ -308,8 +306,7 @@ public class ExampleStreamApi {
     }
 
     private void print(Stream<Employee> stream) {
-        stream
-                .map(emp -> String.format(
+        stream.map(emp -> String.format(
                         "%4d | %-15s %-10s age %s %s",
                         emp.getId(),
                         emp.getLastName(),
@@ -318,7 +315,6 @@ public class ExampleStreamApi {
                         emp.getPosition()
                 ))
                 .forEach(System.out::println);
-
         System.out.println();
     }
 
